fd21344f1e0ba5af9993161bcc153bf312ab31fc
xinmeng <755766021@qq.com>
2017-05-03 16:57:18 +0800

    PDO对象认识与应用

--- pdoDemo.php
+++ pdoDemo.php
@@ -2,29 +2,57 @@
 /**
  * Created by PhpStorm.
  * User: ASUS
- * Date: 2017/5/2
- * Time: 15:15
+ * Date: 2017/5/3
+ * Time: 15:22
  */
 
 
-/**
- *[PDO]
- * pdo.dsn.mysqlpdo = "mysql:host=localhost;dbname=jsp_db"
- *
- */
-
-//$mysqli = new mysqli("localhost","user","password","dbname");
+//1.连接数据库
 try{
-//    1.$pdo = new PDO("mysql:host=localhost;dbname=jsp_db","root","");
-//    2.$pdo = new PDO("uri:mysqlpdo.ini","root","");
-//    3.$pdo = new PDO("mysqlpdo","root","");
-    $pdo = new PDO("mysqlpdo","root","");
-    $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT,0);
+    $pdo=new PDO("mysql:host=localhost;dbname=jsp_db","root","");
 }catch (PDOException $e){
     die("数据库连接失败".$e->getMessage());
 }
-    //echo $pdo->getAttribute(PDO::ATTR_CLIENT_VERSION);
-    //echo $pdo->getAttribute(PDO::ATTR_SERVER_INFO);
-    echo $pdo->getAttribute(PDO::ATTR_AUTOCOMMIT);
-    echo "<br>";
-print_r($pdo);
+//print_r($pdo);
+
+
+//2.执行query(查询)返回的是一个预处理对象
+    $sql = "select * from tbl_user";
+    $stmt = $pdo->query($sql);
+//    $list = $stmt->fetchAll();
+//    $list = $stmt->fetchAll(PDO::FETCH_COLUMN);
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//3.解析数据（一次把所有的数据解析完）
+    foreach($list as $val){
+        echo $val['id']."-------".$val['name']."<br>";
+    }
+//4.释放资源
+    $stmt = null;
+    $pdo = null;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//pdoDemo2↓↓↓↓↓↓↓↓↓
+
+//1.连接数据库
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=jikexueyuan","root","");
+}catch(PDOException $e){
+    die("数据库连接失败".$e->getMessage());
+}
+
+//这是一种快捷的方法
+/* $sql = "select * from stu";
+foreach($pdo->query($sql) as $val){
+	echo $val['id']."-----".$val['name']."<br>";
+} */
+
+//$sql = "insert into stu values(null,'oracle','w',44)";
+//$sql = "delete from stu where id=11";
+$sql = "update stu set name='js' where id=3";
+$res = $pdo->exec($sql);
+if($res){
+    echo "success";
+}
+
